4b4d6165709d861e7542eae568f36cbb23230b9d
jesus <jesuscordovasalcido@gmail.com>
2022-01-28 06:51:18 -0600

    test

---
+++ API Music Project/API Music Project/MusicModel.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+struct genero{
+    
+    var id : Int?
+    var name : String?
+    var picture : String?
+    var picture_small : String?
+    var picture_medium : String?
+    var picture_xl : String?
+    var type : String?
+    
+}
